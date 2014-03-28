ffe469f1f035a7cf81cb7799d4db1ae80423e6fa
John-Titor <msmith@purgatory.org>
2014-03-28 10:58:22 -0700

    documentation

--- common/lin_slave.h
+++ common/lin_slave.h
@@ -12,21 +12,69 @@ class Slave
 public:
     Slave();
 
+    /// Perform one-time initialisation of the slave.
+    ///
     void            init();
 
+    /// Called from the transfer-complete ISR
+    ///
     void            isrTC();
+    
+    /// Called from the error ISR
+    ///
     void            isrError();
 
 protected:
 
+    /// Ask the driver to receive a response.
+    ///
+    /// Subclass implementation of headerReceived will call when an 
+    /// interesting header has been seen; responseReceived will be
+    /// called if a response is actually received.
+    ///
+    ///
+    /// @param length           The length of the response to be received.
+    ///
     void            requestResponse(uint8_t length);
+    
+    
+    /// Send a response.
+    ///
+    /// Subclass implementation of headerReceived will call when it wants ti
+    /// send a response for an interesting header.
+    ///
+    /// Responses are always sent in LIN 2.x format, unless the FID from the
+    /// header implies 1.x (i.e. SlaveResponse).
+    ///
+    /// @param frame            The response frame to send.
+    /// @param length           The length of the response frame.
+    ///
     void            sendResponse(LIN::Frame &frame, uint8_t length);
 
+    /// Called when a header has been received.
+    ///
+    /// Must be implemented by the subclass.
+    ///
+    /// @param fid              The FID from the received header.
+    ///
     virtual void    headerReceived(LIN::FID fid);
+    
+    /// Called when a response requested by requestResponse has been received.
+    ///
+    /// @param fid              The FID from the header associated with this
+    ///                         response.
+    /// @param frame            The received response frame.
+    ///
     virtual void    responseReceived(LIN::FID fid, LIN::Frame &frame);
+    
+    /// Called when transmission of a response is completed.
+    ///
+    /// @param fid              The FID associated with the response that
+    ///                         was sent.
+    ///
     virtual void    responseSent(LIN::FID fid);
 
 private:
-    LIN::FID        _currentFID;
-    LIN::Frame      _frameBuf;
+    LIN::FID        _currentFID;    //< the FID from the most recently received header
+    LIN::Frame      _frameBuf;      //< working buffer for frame data
 };

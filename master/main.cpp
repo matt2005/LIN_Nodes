61cdfffc3453b67aba20e8ae152ea18f739b4fc3
none <none>
2014-05-01 16:19:45 -0700

    Add basic switch handling logic

--- master/main.cpp
+++ master/main.cpp
@@ -5,12 +5,11 @@
 
 #include "board.h"
 #include "lin_protocol.h"
-#include "mc33972.h"
-#include "softserial.h"
 
 #include "display.h"
 #include "master.h"
 #include "menu.h"
+#include "switches.h"
 
 void
 main(void)
@@ -24,7 +23,7 @@ main(void)
 
     // construct drivers that we need
     Master      master;         // XXX must be before switches to get !SS as an output 
-    MC33972     switches;       // XXX should move LINCS somewhere else, use PA6 for SPI !SS
+    Switches    switches;       // XXX should move LINCS somewhere else, use PA6 for SPI !SS
 
     // enable interrupts; timers and LIN events will start.
     sei();
@@ -47,9 +46,95 @@ main(void)
     // run the master logic forever
     for (;;) {
         wdt_reset();
-        //menu.tick();
         switches.scan();
-    }
 
+        // update the relays frame by looking at switches
+        LIN::RelayFrame f;
+
+        // interior light
+        if (switches[LIN::kSWDoor] || switches[LIN::kSWInteriorLight]) {
+            // XXX stay-on timer goes here
+            f.set(LIN::kRelayInteriorLight);
+            // XXX path lighting if ignition was just turned off
+        }
+        if (switches[LIN::kSWHazard]) {
+            // XXX test blink timer
+            f.set(LIN::kRelayLeftTurn);
+            f.set(LIN::kRelayRightTurn);
+        }
+
+        // relays only on with ignition
+        if (switches[LIN::kSWIgnition]) {
+
+            // high/low-beam lights
+            if (switches[LIN::kSWHighBeam]) {
+                f.set(LIN::kRelayHighBeam);
+            } else if (switches[LIN::kSWLowBeam]) {
+                f.set(LIN::kRelayLowBeam);
+            }
+
+            // markers and city lights
+            if (switches[LIN::kSWLights] || 
+                switches[LIN::kSWLowBeam] ||
+                switches[LIN::kSWHighBeam]) {
+                f.set(LIN::kRelayMarkers);
+                f.set(LIN::kRelayCityLights);
+            }
+
+            // foglights
+            // XXX require other lights?
+            if (switches[LIN::kSWFogLight]) {
+                f.set(LIN::kRelayFogLights);
+            }
+
+            // popup headlights 
+            // up when headlights go on, down when all lights go off
+            if (switches[LIN::kSWLowBeam] ||
+                switches[LIN::kSWHighBeam]) {
+                f.set(LIN::kRelayLightsUp);
+                // minimum raise signal time?
+            } else if (!switches[LIN::kSWLights]) {
+                f.set(LIN::kRelayLightsDown);
+                // minimum lower signal time?
+            }
+
+            // turn signals
+            // XXX test keep-blinking timer
+            if (switches[LIN::kSWLeftTurn]) {
+                // XXX test blink timer
+                f.set(LIN::kRelayLeftTurn);
+            }
+            if (switches[LIN::kSWRightTurn]) {
+                // XXX test blink timer
+                f.set(LIN::kRelayRightTurn);
+            }
+
+            // brake lights
+            // XXX test minimum-brake timer
+            // XXX test brake-blnk timer
+            if (switches[LIN::kSWBrake]) {
+                f.set(LIN::kRelayBrake);
+            }
+
+            // reverse lights
+            if (switches[LIN::kSWReverse]) {
+                f.set(LIN::kRelayReverse);
+            }
+
+            // relays only on without ignition
+        } else {
+
+            // parking lights - no blink
+            if (switches[LIN::kSWLeftTurn]) {
+                f.set(LIN::kRelayLeftTurn);
+            }
+            if (switches[LIN::kSWRightTurn]) {
+                f.set(LIN::kRelayRightTurn);
+            }
+        }
+
+        // update the copy we are sending to nodes
+        master.relayFrame.copy(f);
+    }
 }
 

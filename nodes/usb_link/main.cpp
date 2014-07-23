43f65d561867d768da6cab38b472593ea19d3fe8
none <none>
2014-07-22 22:24:04 -0700

    reformat

--- nodes/usb_link/main.cpp
+++ nodes/usb_link/main.cpp
@@ -28,27 +28,31 @@ extern "C" {
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
-usbRequest_t    *rq = (usbRequest_t *)&data[0];
-static uchar    dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup returns */
+    usbRequest_t    *rq = (usbRequest_t *)&data[0];
+    static uchar    dataBuffer[4];  /* buffer must stay valid when usbFunctionSetup returns */
 
-    if(rq->bRequest == CUSTOM_RQ_ECHO){ /* echo -- used for reliability tests */
+    if (rq->bRequest == CUSTOM_RQ_ECHO) { /* echo -- used for reliability tests */
         dataBuffer[0] = rq->wValue.bytes[0];
         dataBuffer[1] = rq->wValue.bytes[1];
         dataBuffer[2] = rq->wIndex.bytes[0];
         dataBuffer[3] = rq->wIndex.bytes[1];
         usbMsgPtr = dataBuffer;         /* tell the driver which data to return */
         return 4;
-    }else if(rq->bRequest == CUSTOM_RQ_SET_STATUS){
-        if(rq->wValue.bytes[0] & 1){    /* set LED */
+
+    } else if (rq->bRequest == CUSTOM_RQ_SET_STATUS) {
+        if (rq->wValue.bytes[0] & 1) {  /* set LED */
             pinLINCS.set();
-        }else{                          /* clear LED */
+
+        } else {                         /* clear LED */
             pinLINCS.clear();
         }
-    }else if(rq->bRequest == CUSTOM_RQ_GET_STATUS){
+
+    } else if (rq->bRequest == CUSTOM_RQ_GET_STATUS) {
         dataBuffer[0] = pinLINCS.get();
         usbMsgPtr = dataBuffer;         /* tell the driver which data to return */
         return 1;                       /* tell the driver to send 1 byte */
     }
+
     return 0;   /* default for not implemented requests: return no data back to host */
 }
 
@@ -68,10 +72,12 @@ main(void)
     usbInit();
     usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
     uint8_t i = 0;
-    while(--i){             /* fake USB disconnect for > 250 ms */
+
+    while (--i) {           /* fake USB disconnect for > 250 ms */
         wdt_reset();
         Board::ms_delay(1);
     }
+
     usbDeviceConnect();
 
     slave.init();
